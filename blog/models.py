a7fe92558c666b9d3d26d38abf627e651483e3ff
gkothyari <gkothyari@gmail.com>
2021-04-27 16:00:04 +0530

    Added Files

---
+++ blog/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+class Answer(models.Model):
+    qid = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='answers')
+    answer_text = models.TextField()
+    date_posted = models.DateTimeField(blank=True)
+    posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.answer_text
